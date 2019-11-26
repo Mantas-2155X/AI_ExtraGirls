b9163ab31f3f49df3f7fc2b58a4e99356f6fbd1b
Mantas <syenergyy@gmail.com>
2019-11-26 12:04:17 +0200

    Change copyright strings

--- AI_ExtraGirls/Properties/AssemblyInfo.cs
+++ AI_ExtraGirls/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
 [assembly: AssemblyProduct("AI_ExtraGirls")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Mantas-2155X aka 2155X Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
